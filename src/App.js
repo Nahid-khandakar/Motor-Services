d50a7c2f338483e4e89038c448b9c2e452696737
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-12 20:27:33 +0600

    routes set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './pages/Shared/Header/Header'
+import Home from './pages/Home/Home/Home'
+import About from './pages/About/About'
+
 
 function App() {
   return (
     <div >
-      <h1>Hello yarn</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
