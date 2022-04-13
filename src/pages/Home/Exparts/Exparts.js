68a5d448c31b7931793768ba2ee28ea3b6766647
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-14 04:34:39 +0600

    image slider add

--- src/pages/Home/Exparts/Exparts.js
+++ src/pages/Home/Exparts/Exparts.js
@@ -7,6 +7,8 @@ import expert4 from '../../../images/experts/expert4.jpg'
 import expert5 from '../../../images/experts/expert5.jpg'
 import expert6 from '../../../images/experts/expert6.png'
 
+import Expart from '../Expart/Expart'
+
 const Exparts = () => {
 
     const exparts = [
@@ -19,8 +21,16 @@ const Exparts = () => {
     ]
 
     return (
-        <div>
-            <h1>this is experts</h1>
+        <div className='container'>
+            <h1 className='text-danger text-center my-3'>this is experts</h1>
+            <div className='row'>
+                {
+                    exparts.map(expart => <Expart
+                        key={expart.id}
+                        expart={expart}
+                    ></Expart>)
+                }
+            </div>
         </div>
     );
 };
