25ed65f49ad8eae8f96bd2ec02730c80d011ea5c
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-15 23:23:24 +0600

    add loading in all page and slove require auth loading problem

--- src/pages/Login/SocialLogin/SocialLogin.js
+++ src/pages/Login/SocialLogin/SocialLogin.js
@@ -5,6 +5,7 @@ import github from '../../../images/social/github.png'
 import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useNavigate } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
 
 
 const SocialLogin = () => {
@@ -19,6 +20,10 @@ const SocialLogin = () => {
         errorElement = <div>
             <p className='text-danger'>Error: {error?.message} {error1?.message}</p>
         </div>
+    }
+
+    if (loading || loading1) {
+        return <Loading></Loading>
     }
 
     if (user || user1) {
