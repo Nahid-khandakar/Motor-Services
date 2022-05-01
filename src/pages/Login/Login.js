82fe40806fc07f5e2b0cdda5a432e8cffe8d9f80
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-05-01 18:14:58 +0600

    jwt add

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -11,6 +11,7 @@ import SocialLogin from './SocialLogin/SocialLogin';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Helmet } from 'react-helmet-async';
+import axios from 'axios';
 
 const Login = () => {
 
@@ -32,12 +33,18 @@ const Login = () => {
 
 
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value
         const password = passwordRef.current.value
         //console.log(email, password)
-        signInWithEmailAndPassword(email, password)
+        await signInWithEmailAndPassword(email, password)
+
+
+        const { data } = await axios.post('http://localhost:5000/login', { email })
+        //console.log(data.assessToken)
+        localStorage.setItem('accessToken', data.accessToken)
+        navigate(from, { replace: true });
 
     }
     let errorElement;
@@ -51,7 +58,7 @@ const Login = () => {
     }
 
     if (user) {
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
     }
 
 
